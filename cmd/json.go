552f8bf7e4d57658dcbfad12a30770bfb6b7aeb1
maia arson crimew <maia@crimew.gay>
2022-06-06 13:02:39 +0200

    json command: select between indoor and outdoor data

--- cmd/json.go
+++ cmd/json.go
@@ -15,7 +15,7 @@ var jsonCmd = &cobra.Command{
 	Use:     "json",
 	Short:   "read data from netatmo station as json",
 	Long:    `read data from netatmo station as json`,
-	Example: "netatmo json",
+	Example: "netatmo json -i",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		netatmoClient, err := netatmo.NewClient(netatmo.Config{
 			ClientID:     viper.GetString("netatmo.clientID"),
@@ -28,22 +28,31 @@ var jsonCmd = &cobra.Command{
 			return err
 		}
 
-		if len(args) > 0 {
+		if indoor {
+			printIndoorJson(netatmoClient.GetStationData())
+		} else if outdoor {
+			printOutdoorJson(netatmoClient.GetStationData())
+		} else {
 			fmt.Println(cmd.UsageString())
-			return nil
-		} 
-
-		printJson(netatmoClient.GetStationData())
+		}
 
 		return nil
 	},
 }
 
-func printJson(stationData netatmo2.StationData) {
+func printIndoorJson(stationData netatmo2.StationData) {
 	data, _ := json.Marshal(stationData.Body.Devices[0].DashboardData)
+	fmt.Println(string(data))
+}
+
+func printOutdoorJson(stationData netatmo2.StationData) {
+	data, _ := json.Marshal(stationData.Body.Devices[0].Modules[0].DashboardData)
 	fmt.Println(string(data))
 }
 
 func init() {
 	rootCmd.AddCommand(jsonCmd)
+
+	jsonCmd.Flags().BoolVarP(&indoor, "indoor", "i", false, "netatmo json -i|--indoor")
+	jsonCmd.Flags().BoolVarP(&outdoor, "outdoor", "o", false, "netatmo json -o|--outdoor")
 }
